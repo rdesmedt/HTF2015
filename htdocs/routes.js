7b64e064fa643db6f201f8b45c65249f4a1a104f
Roeland De Smedt <roeland.desmedt@c4j.be>
2015-11-29 02:36:12 +0100

    fancy message

--- htdocs/routes.js
+++ htdocs/routes.js
@@ -3,7 +3,7 @@
 module.exports = function(app) {
 
   app.get('/', function(req, res){
-    res.status(200).send('Hello');
+    res.status(200).send('Hello, is it me you\'re looking for?');
   });
 
 };
