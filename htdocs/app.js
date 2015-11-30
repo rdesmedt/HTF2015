700861cdff16adc221bd2cb8b4395e774e645446
Roeland De Smedt <roeland.desmedt@c4j.be>
2015-11-30 20:48:47 +0100

    body-parser added

--- htdocs/app.js
+++ htdocs/app.js
@@ -7,7 +7,7 @@ var express = require('express'),
 var app = express();
 
 app.set('views engine', 'jade');
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/css', express.static(__dirname + '/public/css'));
 
